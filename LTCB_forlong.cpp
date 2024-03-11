a4ef3c2251a90a4139605f16f4ff4d1b68918494
ADMIN <ADMIN@LAB1-MAY28>
2024-03-11 11:21:31 +0700

    Add project files.

---
+++ LTCB_forlong.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout << "nhap 3 so nguyen:"; cin >> a >> b >> c;
+	int max = a;
+	max = b;
+
+	if (c > max) {
+		max = c;
+	}
+	int min = a;
+	if (b < min) {
+		min = b;
+	}
+	if (c < min) {
+		min = c;
+		cout << "so lon nhat la:" << max << endl;
+		cout << "so nho nhat la""<<" << min << endl;
+		return 0;
+
+
+	}
+
+
+}
